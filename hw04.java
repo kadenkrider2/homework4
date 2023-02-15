67fa94188865290ce69efb20dee4daebbdede91d
kadenkrider2 <118944863+kadenkrider2@users.noreply.github.com>
2023-02-15 13:47:13 -0500

    Create hw04.java

---
+++ hw04.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class hw04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer with no more than five digits: ");
+        int number = input.nextInt();
+
+        // Convert the integer to a string
+        String numberString = Integer.toString(number);
+
+        // Print the digits with spaces between them
+        for (int i = 0; i < numberString.length(); i++) {
+            System.out.print(numberString.charAt(i) + " ");
+        }
+    }
+}
