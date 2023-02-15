f5cda9a63a3d23163eadda7972fdce09d1aa3d42
kadenkrider2 <118944863+kadenkrider2@users.noreply.github.com>
2023-02-15 13:49:06 -0500

    Update hw04.java

--- hw04.java
+++ hw04.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class hw04 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter an integer with no more than five digits: ");
+        System.out.print("PLease enter a sequence of integers no longer than five digits: ");
         int number = input.nextInt();
 
         // Convert the integer to a string
